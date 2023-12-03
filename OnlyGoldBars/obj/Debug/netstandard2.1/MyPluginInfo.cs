0260be0b0cb67eeae03e8e858a89a43b5030353b
Cody Thompson <cody3thomp@gmail.com>
2023-12-02 20:40:53 -0800

    Everything compiled in VS

---
+++ OnlyGoldBars/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace OnlyGoldBars
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "OnlyGoldBars";
+        public const string PLUGIN_NAME = "OnlyGoldBars";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
