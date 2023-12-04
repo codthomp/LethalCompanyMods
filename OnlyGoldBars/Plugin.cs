60e5820f482d81fe1a75822d1c3f35bf4d988d92
Cody Thompson <cody3thomp@gmail.com>
2023-12-03 22:01:30 -0800

    Maps that don't usually have gold bars now spawn gold bars

--- OnlyGoldBars/Plugin.cs
+++ OnlyGoldBars/Plugin.cs
@@ -10,7 +10,7 @@ namespace OnlyGoldBars
         public static bool loaded;
         private const string modGUID = "Alphonso.OnlyGoldBars";
         private const string modName = "OnlyGoldBars";
-        private const string modVersion = "1.0.1";
+        private const string modVersion = "1.0.3";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         private static OnlyGoldBars Instance;
@@ -28,14 +28,28 @@ namespace OnlyGoldBars
         [HarmonyPrefix]
         static bool Only_Gold_Bars(ref SelectableLevel newLevel)
         {
-            HUDManager.Instance.AddTextToChatOnServer("<color=red>All items are gold bars</color>");
-
+            bool goldFound = false;
             foreach (var item in newLevel.spawnableScrap)
             {
                 item.rarity = 0;
                 if (item.spawnableItem.itemName == "Gold bar")
                 {
                     item.rarity = 999;
+                    goldFound = true;
+                }
+            }
+            if (goldFound == false)
+            {
+                StartOfRound NewRound = new StartOfRound();
+                foreach (var item in StartOfRound.Instance.allItemsList.itemsList)
+                {
+                    if (item.itemName == "Gold bar")
+                    {
+                        SpawnableItemWithRarity newItem = new SpawnableItemWithRarity();
+                        newItem.spawnableItem = item;
+                        newItem.rarity = 999;
+                        newLevel.spawnableScrap.Add(newItem);
+                    }
                 }
             }
             return true;
